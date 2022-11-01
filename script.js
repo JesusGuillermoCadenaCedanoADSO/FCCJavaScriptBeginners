facfa72c8ba2c9f3e5afbdd21a43234c05062d5e
JesusGuillermoCadenaCedanoADSO <cadena.jesus@gmail.com>
2022-10-31 21:42:17 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hola mundo");
+var number=5; // in-line comment
+/* this is a multiline
+comment */
+/* Data types:
+undefined, null, boolean, string, symbol, number and object
+*/
+var myName = "Jesus" //tiene alcance en todo el programa
+
+let ourName = "freeCodeCamp" //tiene alcance limitado
+
+const pi=3.14
+
+//Declaración de variables
+
